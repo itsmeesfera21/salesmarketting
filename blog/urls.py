1c41c34ac7b2ed720f324ad2dfc6eaa47f0f15be
Click Ripple <clickripple.master@gmail.com>
2016-10-24 15:23:00 +0530

    Implemented delete and publish functionality

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^posts/$', views.post_all, name='post_all'),
+    url(r'^post/(?P<slug>.*)/edit/$', views.post_edit, name='post_edit'),
+    url(r'drafts/$',views.post_draft_list,name="post_draft_list"),
+    url(r'^drafts/publish/(?P<slug>.*)/',views.post_publish,name='post_publish'),
+    url(r'post/new/$',views.post_new,name="post_new"),
+    url(r'post/delete/(?P<slug>.*)/',views.post_delete,name="post_delete"),
     url(r'^post/(?P<slug>.*)/',views.post_detail,name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
+from django.shortcuts import redirect
 import json
+from django.template.defaultfilters import slugify
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -18,6 +21,50 @@ def post_all(request):
     	postss.append({"title":post.title,"author":post.author_id})
     return HttpResponse(str(postss))
     #return render(request, 'blog/post_list.html', {'posts': posts})    
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        print request.POST
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            print  request.user
+            #post.published_date = timezone.now()
+            #post.slug='%s'%(slugify(request.POST))
+            post.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+	posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+def post_publish(request,slug):
+	post=get_object_or_404(Post,slug=slug)
+	post.publish()
+	return redirect('post_detail', slug=post.slug)
+
+def post_delete(request,slug):
+	post=get_object_or_404(Post,slug=slug)
+	post.delete()
+	return redirect('post_list')
+
+def post_edit(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
 def post_detail(request, slug):
